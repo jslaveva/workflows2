a53a27082e0bac0f7b0404f4160a5fa19098d8c2
unknown <you@example.com>
2015-03-31 20:53:46 +0300

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee=require('gulp-coffee');
 
+var coffeeSources = ['components/coffee/tagline.coffee']
 gulp.task('coffee',function(){
-    gulp.src('components/coffee/tagline.coffee')
+    gulp.src(coffeeSources)
         .pipe(coffee({bare:true})
             .on('error',gutil.log))
-        .pipe()
+        .pipe(gulp.dest('components/scripts')
     )
 });
